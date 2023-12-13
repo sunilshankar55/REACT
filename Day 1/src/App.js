a85c7779e1b0e088af470b0140aa3c87c8fcef27
sunilshankar55 <120124490+sunilshankar55@users.noreply.github.com>
2023-12-13 15:02:50 +0530

    Add files via upload

---
+++ Day 1/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginPage from './components/LoginPage';
+import './app.css';
+function App(){
+  return (
+    <div>
+      < LoginPage />
+    </div>
+  )
+}
+export default App;
